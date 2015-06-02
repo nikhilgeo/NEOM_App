688a77207dc6f4fcd5d37fdf5ab5397603e8ffa9
Nikhil George <nikhilgeo@gmail.com>
2015-06-02 21:25:35 +0530

    Data type chnaged

--- NEOM/src/com/nikhil/neom/Model_Process.java
+++ NEOM/src/com/nikhil/neom/Model_Process.java
@@ -14,4 +14,8 @@ public class Model_Process {
 	public Integer processID;
 	public Integer userID;
 	public String packageList;
+	public String AppName;
+	public float[] memInfo;// [0] totalpss, [1] totalprivateDirty, [2]
+							// totalsharedDirty
+
 }
