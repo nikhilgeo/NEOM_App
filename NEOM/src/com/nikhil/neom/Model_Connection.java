29000d5da30191a3b06561a1ec7ef610d34584e1
Nikhil George <nikhilgeo@gmail.com>
2015-06-05 16:45:49 +0530

    Model class for connection

---
+++ NEOM/src/com/nikhil/neom/Model_Connection.java
@@ -0,0 +1,11 @@
+package com.nikhil.neom;
+
+import java.util.ArrayList;
+
+public class Model_Connection {
+	String appName;
+	Integer processID;
+	String sndrev;
+	String connections;
+	ArrayList<Utilities.Connection> con;
+}
