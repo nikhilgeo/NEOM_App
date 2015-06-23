0ba5de15994c38912f09278ee5b47228101bb820
Nikhil George <nikhilgeo@gmail.com>
2015-06-23 17:21:57 +0530

    DB helper class and functions

---
+++ NEOM/src/com/nikhil/neom/iptablesDBContract.java
@@ -0,0 +1,17 @@
+package com.nikhil.neom;
+
+import android.provider.BaseColumns;
+
+public class iptablesDBContract {
+	// To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public iptablesDBContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class iptblrule implements BaseColumns {
+        public static final String TABLE_NAME = "iptblrules";
+        public static final String COLUMN_NAME_UID = "uid";
+        public static final String COLUMN_NAME_RULE = "rule";
+    }
+   
+}
