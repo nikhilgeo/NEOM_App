584d286a4f58026b8b1b867dd32d006e4fdfc7f2
Nikhil George <nikhilgeo@gmail.com>
2015-06-23 17:22:09 +0530

    DB helper class and functions

---
+++ NEOM/src/com/nikhil/neom/neomDbHelper.java
@@ -0,0 +1,47 @@
+package com.nikhil.neom;
+
+import com.nikhil.neom.iptablesDBContract.iptblrule;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class neomDbHelper extends SQLiteOpenHelper {
+	// If you change the database schema, you must increment the database
+	// version.
+	public static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "neom.db";
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String COMMA_SEP = ",";
+	private static final String SQL_CREATE_ENTRIES = "CREATE TABLE "
+			+ iptblrule.TABLE_NAME + " (" + iptblrule._ID
+			+ " INTEGER PRIMARY KEY," + COMMA_SEP + iptblrule.COLUMN_NAME_UID + TEXT_TYPE
+			+ COMMA_SEP + iptblrule.COLUMN_NAME_RULE + TEXT_TYPE + " )";
+
+	private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
+			+ iptblrule.TABLE_NAME;
+
+	public neomDbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(SQL_CREATE_ENTRIES);
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// This database is only a cache for online data, so its upgrade policy
+		// is
+		// to simply to discard the data and start over
+		db.execSQL(SQL_DELETE_ENTRIES);
+		onCreate(db);
+	}
+
+	public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		onUpgrade(db, oldVersion, newVersion);
+	}
+}
