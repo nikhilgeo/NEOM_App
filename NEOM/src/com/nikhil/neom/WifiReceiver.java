29b5f7356a186669ac245457a041ca65f2745766
Nikhil George <nikhilgeo@gmail.com>
2015-06-25 00:17:51 +0530

    android.net.wifi.STATE_CHANGE broadcast receiver added

---
+++ NEOM/src/com/nikhil/neom/WifiReceiver.java
@@ -0,0 +1,45 @@
+package com.nikhil.neom;
+
+import java.util.List;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class WifiReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		NetworkInfo info = intent
+				.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+		if (info != null) {
+			WifiManager wifiManager = (WifiManager) context
+					.getSystemService(Context.WIFI_SERVICE);
+			WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+			String ssid = wifiInfo.getSSID();
+			if (info.isConnected()) {
+
+				Toast.makeText(context, "Connected to WiFi " + ssid,
+						Toast.LENGTH_SHORT).show();
+				// fetch the rules for SSID from DB
+				// APPEND the rules to iptable
+
+			}
+			if (!info.isConnected()) {
+				// When ever disconnected remove the applied WiFi
+				// context----Save some flag in DB to knw if rules are on--remove rules only if they are applied.
+				
+				Toast.makeText(context, "Disconnected from WiFi " + ssid,
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+}
