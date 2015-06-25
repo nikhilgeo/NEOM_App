95a23daee3274302f8735cd0e354e40a6784085b
Nikhil George <nikhilgeo@gmail.com>
2015-06-26 00:31:50 +0530

    Set Context list and add wifi btn added

--- NEOM/src/com/nikhil/neom/SetContextFragment.java
+++ NEOM/src/com/nikhil/neom/SetContextFragment.java
@@ -1,12 +1,19 @@
 package com.nikhil.neom;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.ListFragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass. Activities that contain this fragment
@@ -14,37 +21,15 @@ import android.view.ViewGroup;
  * interface to handle interaction events. Use the
  * {@link SetContextFragment#newInstance} factory method to create an instance
  * of this fragment.
- *
+ * 
  */
-public class SetContextFragment extends Fragment {
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
+public class SetContextFragment extends ListFragment {
 
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
+	String[] numbers_text = new String[] { "one", "two", "three", "four",
+			"two", "three", "two", "three", "two", "three", "five" };
 
-	private OnFragmentInteractionListener mListener;
-
-	/**
-	 * Use this factory method to create a new instance of this fragment using
-	 * the provided parameters.
-	 *
-	 * @param param1
-	 *            Parameter 1.
-	 * @param param2
-	 *            Parameter 2.
-	 * @return A new instance of fragment SetContextFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
-	public static SetContextFragment newInstance(String param1, String param2) {
+	public static SetContextFragment newInstance() {
 		SetContextFragment fragment = new SetContextFragment();
-		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
-		fragment.setArguments(args);
 		return fragment;
 	}
 
@@ -55,56 +40,64 @@ public class SetContextFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
+
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater
-				.inflate(R.layout.fragment_set_context, container, false);
+
+		View rootView = inflater.inflate(R.layout.fragment_set_context,
+				container, false);
+		Button btnAddSSID = (Button) rootView.findViewById(R.id.btnAddSSID);
+		final TextView txtSSID = (TextView) rootView.findViewById(R.id.txtSSID);
+		btnAddSSID.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				String ssid = txtSSID.getText().toString();
+				
+
+				if (ssid.isEmpty())
+					Toast.makeText(getActivity(), "Need WiFi SSID" + ssid,
+							Toast.LENGTH_SHORT).show();
+				else
+				{
+					FragmentManager fragmentManager = getFragmentManager();
+					fragmentManager
+							.beginTransaction()
+							.replace(R.id.container,
+									FilterFragment.newInstance(ssid))
+							.addToBackStack("FrmFilter").commit();
+				}
+			}
+		});
+		return rootView;
 	}
 
-	// TODO: Rename method, update argument and hook method into UI event
-	public void onButtonPressed(Uri uri) {
-		if (mListener != null) {
-			mListener.onFragmentInteraction(uri);
-		}
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+
+		super.onActivityCreated(savedInstanceState);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, numbers_text);
+
+		setListAdapter(adapter);
+
 	}
 
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
-		try {
-			mListener = (OnFragmentInteractionListener) activity;
-		} catch (ClassCastException e) {
-			throw new ClassCastException(activity.toString()
-					+ " must implement OnFragmentInteractionListener");
-		}
+
 	}
 
 	@Override
 	public void onDetach() {
 		super.onDetach();
-		mListener = null;
-	}
 
-	/**
-	 * This interface must be implemented by activities that contain this
-	 * fragment to allow an interaction in this fragment to be communicated to
-	 * the activity and potentially other fragments contained in that activity.
-	 * <p>
-	 * See the Android Training lesson <a href=
-	 * "http://developer.android.com/training/basics/fragments/communicating.html"
-	 * >Communicating with Other Fragments</a> for more information.
-	 */
-	public interface OnFragmentInteractionListener {
-		// TODO: Update argument type and name
-		public void onFragmentInteraction(Uri uri);
 	}
 
 }
